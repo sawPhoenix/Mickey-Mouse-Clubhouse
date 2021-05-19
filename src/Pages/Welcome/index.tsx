862d52df55a968188e52f34136b01e5bfb86961b
whosyourdaddy <56509282+sawPhoenix@users.noreply.github.com>
2021-05-19 21:23:28 +0800

    change bg

--- src/Pages/Welcome/index.tsx
+++ src/Pages/Welcome/index.tsx
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 import Button from '../../components/PublicComponents/Button'
 import classnames from "classnames";
 const WolCome: React.FC = () => {
-  const [background, setBackground] = useState(Math.floor(Math.random()*30)+ 1);
+  const [background, setBackground] = useState(Math.floor(Math.random() * 30) + 1);
 
   const className_bg = classnames("welcome_container", {
     [`bg${background}`]: true
   })
   return (
     <div className={className_bg}  >
-      <Button btnType="neon" neonColor="red">test</Button>
+      <div className="welcome_changebg_btn">
+        <Button btnType="neon" onClick={() => setBackground(background + 1)}>切换背景</Button>
+      </div>
     </div>
   )
 }
