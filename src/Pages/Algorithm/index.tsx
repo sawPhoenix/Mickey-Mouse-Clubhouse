245d504890b935415985c705dee35ab4444add08
whosyourdaddy <56509282+sawPhoenix@users.noreply.github.com>
2021-06-06 17:48:44 +0800

    save

--- src/Pages/Algorithm/index.tsx
+++ src/Pages/Algorithm/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Button from '@components/PublicComponents/Button';
+import Button from '../../components/PublicComponents/Button';
 import { Queue, Stack, LinkedList } from "./pubic"; //  数据结构实现
 import unionFind from "./example//unionFind";
 import sort from "./example//sort";
@@ -24,6 +24,7 @@ const Algorithm: React.FC = () => {
   return (
     <div>
       {selectionSort}
+
     </div>
   )
 }
