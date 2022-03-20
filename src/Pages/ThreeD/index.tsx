b0b8992a1292275c6dbd24356b0fb2be71bbb321
whosyourdaddy <56509282+sawPhoenix@users.noreply.github.com>
2022-03-20 15:07:03 +0800

    feat: add three.js

--- src/Pages/ThreeD/index.tsx
+++ src/Pages/ThreeD/index.tsx
@@ -1,13 +1,28 @@
 import { useRef } from 'react';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame, MeshProps } from '@react-three/fiber';
+const Box = () => {
+  const ref = useRef<MeshProps>({});
+  useFrame(state => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={ref}>
+      <boxBufferGeometry />
+      <meshBasicMaterial color="blue" />
+    </mesh>
+  );
+};
 export default function () {
   return (
-    <div>
-      <Canvas style={{ background: 'black' }}>
-        <mesh>
-          <boxBufferGeometry />
-          <meshBasicMaterial color="blue" />
-        </mesh>
+    <div style={{ width: '100%', height: '100%' }}>
+      <Canvas
+        style={{ width: '100%', height: '100%', background: 'black' }}
+        camera={{ position: [3, 3, 3] }}
+      >
+        {/* <Box /> */}
+        <axesHelper args={[5]} />
       </Canvas>
     </div>
   );
